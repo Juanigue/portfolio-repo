f6b392dc6830ec9a7ee4fade83142c2bcfe8d56e
Juan Ignacio Guerrero <49444608+Juanigue@users.noreply.github.com>
2023-05-24 11:14:45 -0300

    Delete next-portfolio-app directory

--- next-portfolio-app/app/admin/[[...index]]/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client"; //This is to tell next to render on the browser on the client, not the server
-
-import config from '@juanigue/sanity.config';
-import { NextStudio } from 'next-sanity/studio';
-
-
-export default function AdminPage() {
-  return <NextStudio config={config} />;
-}
